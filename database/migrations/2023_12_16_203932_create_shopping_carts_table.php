c823c44b67420d1e8418f4470695da4b405c61ad
yusufarya <aryaherby29nov2k@gmail.com>
2023-12-17 00:56:11 +0700

    create shopping_carts table

---
+++ database/migrations/2023_12_16_203932_create_shopping_carts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('shopping_carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('customer_code');
+            $table->foreign('customer_code')->on('customers')->references('code');
+            $table->unsignedBigInteger('sales_order_id');
+            $table->foreign('sales_order_id')->on('sales_orders')->references('id');
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->on('products')->references('id');
+            $table->double('qty')->default(0);
+            $table->double('price')->default(0);
+            $table->dateTime('created_at')->nullable()->default(date('Y-m-d H:i:s'));
+            $table->dateTime('updated_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('shopping_carts');
+    }
+};
